c4e667c6097786f9f3eed9ee1e25dc9ac393d293
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-04-06 12:39:27 +0200

    commit to install OZ libraries

---
+++ smartcontract/src/TransferCurrency.sol
@@ -0,0 +1,10 @@
+//TransferCurrency.sol: Transfer currencies between associated.abi
+//Just transfer the main currency of the network (e.g matic, eth)
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.9;
+
+
+contract TransferCurrency {
+    
+}
